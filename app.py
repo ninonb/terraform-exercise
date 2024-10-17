2aec3951b88c204272ab89ce0b0e157f05091045
ninonb <ninonbecquart@gmail.com>
2024-10-17 09:50:11 +0200

    api

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "The dancing plague of 1518 occurred in Strasbourg from July 1518 to September 1518."}
+
